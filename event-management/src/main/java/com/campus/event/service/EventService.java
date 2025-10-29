a0b6fc8637c5180e5d7e2ecc4ae603e4212e1d28
Karnulu Suresh <karnulusuresh729@gmail.com>
2025-10-29 11:32:11 +0530

    updated README.md file

--- event-management/src/main/java/com/campus/event/service/EventService.java
+++ event-management/src/main/java/com/campus/event/service/EventService.java
@@ -7,10 +7,15 @@ import com.campus.event.entity.Event;
 public interface EventService {
 	
 	List<Event> getAllEvents();
+	
     Event getEventById(Long id);
+    
     List<Event> getEventsByCategory(Long categoryId);
+    
     Event createEvent(Event event);
+    
     Event updateEvent(Long id, Event event);
+    
     void deleteEvent(Long id);
 
 }
